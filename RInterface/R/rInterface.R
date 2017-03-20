3eaffaa22baf197d119d27aa7c5d8a83215b55d3
Wazeer Zulfikar <wazeer.zulfikar@gmail.com>
2017-03-20 12:51:18 +0530

    Added R Interface

---
+++ RInterface/R/rInterface.R
@@ -0,0 +1,4 @@
+library(rPython)
+python.load('../Decision_Tree.py')
+output <- 'Decision Tree Loaded'
+print(output)
---
+++ rInterface.R
@@ -0,0 +1,3 @@
+library(rPython)
+python.load('../Decision_Tree.py')
+print('Decision Tree Loaded')
